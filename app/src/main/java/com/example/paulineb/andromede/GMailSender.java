c0f1be4324e4e55a1b82d2549465e955b11b380a
PaulineSolheim <paulinebouffioux@gmail.com>
2018-05-24 12:34:29 +0200

    envoi d' email reussi cd Andromede

---
+++ app/src/main/java/com/example/paulineb/andromede/GMailSender.java
@@ -0,0 +1,10 @@
+package com.example.paulineb.andromede;
+
+import javax.mail.Authenticator;
+
+/**
+ * Created by PaulineB on 17/05/18.
+ */
+
+public class GMailSender extends Authenticator {
+}
---
+++ app/src/main/java/com/example/paulineb/andromede/JSSEProvider.java
@@ -0,0 +1,10 @@
+package com.example.paulineb.andromede;
+
+import provider;
+
+/**
+ * Created by PaulineB on 17/05/18.
+ */
+
+public class JSSEProvider extends provider {
+}
